f637051472705d655dba6cb6fa04e92e315737a1
shamimice03 <shamimice03@gmail.com>
2024-05-09 00:41:02 +0900

    api improvement part 1

--- app.py
+++ app.py
@@ -1,58 +1,76 @@
 from flask import Flask, request
-import jsonify
+from db import stores, items
+from flask_smorest import abort
+import uuid
+
 
 app = Flask(__name__)
-stores = [
-    {
-        "name": "My Store", 
-        "items": [
-            {
-            "name": "My Item", 
-            "price": 15.99
-            }
-        ]
-    }
-]
 
-@app.get('/store')
+
+@app.get("/store")
 def get_stores():
-    return stores
+    return {"stores": list(stores.values())}
 
-@app.post('/store')
+
+@app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {
-        "name": request_data["name"],
-        "items": []
-    }
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
 
-@app.post('/store/<string:name>/items')
-def create_item_in_store(name):
-    request_data = request.get_json()
-    print(request_data)
-    for store in stores:
-        if store["name"] == name:
-            for item in request_data:
-                new_item = {
-                    "name": item["name"],
-                    "price": item["price"]
-                }
-                store["items"].append(new_item)
-            return store, 201
-    return {"message": "Store not found"}, 404
+    if "name" not in store_data:
+        abort(400, message="Bad request. Missing 'name' field.")
 
-@app.get('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message": "Store not found"}, 404
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="Store already exists")
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
-@app.get('/store/<string:name>/items')
-def get_items_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store["items"]
-    return {"message": "Store not found"}, 404
+
+@app.post("/item")
+def create_item_in_store():
+    item_data = request.get_json()
+
+    if (
+        "store_id" not in item_data
+        or "price" not in item_data
+        or "name" not in item_data
+    ):
+        abort(400, message="Bad request. Missing required fields.")
+
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message="Item already exists")
+
+    if item_data["store_id"] not in stores:
+        abort(404, message="Store not found")
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item, 201
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found")
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found")
---
+++ db.py
@@ -0,0 +1,8 @@
+stores = {}
+items = {}
+
+# items = {
+#     1: {"name": "TV", "price": 15.99},
+#     2: {"name": "Radio", "price": 10.99},
+#     3: {"name": "Cycle", "price": 199.23},
+# }
